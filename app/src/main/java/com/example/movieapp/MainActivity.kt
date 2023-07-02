2101063559e40f954bd1b428607604c264fe863a
tejaswibhagat <107565043+tejaswibhagat@users.noreply.github.com>
2023-07-02 21:29:49 +0530

    MovieApp

---
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.movieapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.movieapp.ui.theme.MovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                MainContent()
+            }
+        }
+    }
+}
+@Composable
+fun MyApp(content: @Composable () -> Unit){
+    MovieAppTheme {
+        content()
+
+    }
+}
+
+@Composable
+fun MainContent(){
+    Surface(color = MaterialTheme.colors.background) {
+        
+    }
+    Text(text = "hello")
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        MainContent()
+    }
+}
