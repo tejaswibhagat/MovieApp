22f4f40845dab4dc7ee9c0f683927486c3e0f2af
tejaswibhagat <107565043+tejaswibhagat@users.noreply.github.com>
2023-07-07 15:02:09 +0530

    MovieApp

--- app/src/main/java/com/example/movieapp/Screens/home/details/DetailsScreen.kt
+++ app/src/main/java/com/example/movieapp/Screens/home/details/DetailsScreen.kt
@@ -1,17 +1,22 @@
 package com.example.movieapp.Screens.home.details
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -26,13 +31,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.example.movieapp.model.Movie
+import com.example.movieapp.model.getMovies
+import com.example.movieapp.widgets.MovieRow
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun DetailsScreen(
     navController: NavController,
-    movieData: String?
+    movieID: String?
 ) {
+    val newMovieList = getMovies().filter { movie ->
+
+        movie.id == movieID
+    }
 
     Scaffold(topBar = {
         TopAppBar(
@@ -54,13 +67,15 @@ fun DetailsScreen(
         Surface(modifier = Modifier.fillMaxSize()) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
             ) {
+                MovieRow(movie = newMovieList.first())
 
-                if (movieData != null) {//different approch if error occurs its here
-                    Text(text = movieData,
-                        style = MaterialTheme.typography.h5)
-                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
+                Text(text = "Movie Image")
+                HorizontalScrollableImageView(newMovieList)
+
 
             }
 
@@ -68,4 +83,24 @@ fun DetailsScreen(
 
     }
 
+}
+
+@Composable
+private fun HorizontalScrollableImageView(newMovieList: List<Movie>) {
+    LazyRow {
+        items(newMovieList[0].images) { image ->
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp),
+                elevation = 5.dp
+            ) {
+                Image(
+                    painter = rememberImagePainter(data = image),
+                    contentDescription = "Movie Hoster"
+                )
+            }
+
+        }
+    }
 }
--- app/src/main/java/com/example/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/example/movieapp/widgets/widgets.kt
@@ -1,5 +1,6 @@
 package com.example.movieapp.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -12,18 +13,31 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
 import com.example.movieapp.model.Movie
@@ -35,11 +49,14 @@ fun MovieRow(
     movie: Movie = getMovies()[0],
     onItemClick: (String) -> Unit = {}
 ) {
+    var expanded by remember{
+        mutableStateOf(false)
+    }
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
+//            .height(130.dp)
             .clickable {
                 onItemClick(movie.id)
             },
@@ -82,8 +99,43 @@ fun MovieRow(
                 Text(text = "Release: ${movie.year}",
                     style = MaterialTheme.typography.caption
                 )
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        Text(buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.Gray,
+                            fontSize = 13.sp)){
+                                append("Plot: ")
+                            }
+                            withStyle(style = SpanStyle(color = Color.Gray,
+                            fontSize = 13.sp,
+                            fontWeight = FontWeight.Light)){
+                                append(movie.plot)
+                            }
+                        }, modifier = Modifier.padding(6.dp))
+
+                        Divider(modifier = Modifier.padding(3.dp))
+                        Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.caption)
+                        Text(text = "Actors: ${movie.actors}", style = MaterialTheme.typography.caption)
+                        Text(text = "Rating: ${movie.rating }", style = MaterialTheme.typography.caption)
+
+                    }
+                }
+
+
+                Icon(imageVector = if(expanded) Icons.Filled.KeyboardArrowUp
+                    else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        },
+                    tint = Color.DarkGray
+                )
 
             }
+
+
         }
     }
 }
